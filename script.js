5fb2396ddda07f27ba9f3a7ca5f79384d2eee140
Patrick <patrickconnors@college.harvard.edu>
2016-07-15 17:06:30 -0400

    splash page (brothers)

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 500);
+  });
+}
+
 $( document ).ready(function() {
     $('#test').html("<p>hello</p>");
+
+    $('#about-nav').click(function (event) {
+      event.preventDefault();
+      $('#stuff').scrollView();
+    });
 });
