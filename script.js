6da079a5c1554134c49ce810556111921375fd29
Patrick <patrickconnors@college.harvard.edu>
2016-07-18 15:09:03 -0400

    FAQ + Courses+ Team

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ $.fn.scrollView = function () {
 }
 
 $( document ).ready(function() {
-    $('#test').html("<p>hello</p>");
-
+    //Menu scrolling
     $('#about-nav').click(function (event) {
       event.preventDefault();
       $('#about-nav-section').scrollView();
@@ -28,5 +27,55 @@ $( document ).ready(function() {
     $('#contact-nav').click(function (event) {
       event.preventDefault();
       $('#contact-nav-section').scrollView();
+    });
+
+    //Course Section
+    $('#full-stack-section').click(function(event){
+      event.preventDefault();
+      $('#full-stack-section').html('<p>Test</p>');
+      $(this).removeClass('hvr-grow');
+      console.log('hello');
+    });
+
+    //FAQ Section
+    $('#cost-question').click(function(event){
+      event.preventDefault();
+      $('#cost-answer').slideDown(200);
+      $('#benefit-answer').hide();
+      $('#time-answer').hide();
+      $('#different-answer').hide();
+      $('#teacher-answer').hide();
+    });
+    $('#benefit-question').click(function(event){
+      event.preventDefault();
+      $('#cost-answer').hide();
+      $('#benefit-answer').slideDown(200);
+      $('#time-answer').hide();
+      $('#different-answer').hide();
+      $('#teacher-answer').hide();
+    });
+    $('#time-question').click(function(event){
+      event.preventDefault();
+      $('#cost-answer').hide();
+      $('#benefit-answer').hide();
+      $('#time-answer').slideDown(200);
+      $('#different-answer').hide();
+      $('#teacher-answer').hide();
+    });
+    $('#different-question').click(function(event){
+      event.preventDefault();
+      $('#cost-answer').hide();
+      $('#benefit-answer').hide();
+      $('#time-answer').hide();
+      $('#different-answer').slideDown(200);
+      $('#teacher-answer').hide();
+    });
+    $('#teacher-question').click(function(event){
+      event.preventDefault();
+      $('#cost-answer').slideUp(1);
+      $('#benefit-answer').slideUp(1);
+      $('#time-answer').slideUp(1);
+      $('#different-answer').slideUp(1);
+      $('#teacher-answer').slideDown(200);
     });
 });
